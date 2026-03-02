9bff9a5a064836364006e5b921e07e27abad3230
Fy Raza <gasyevolve@gmail.com>
2026-03-02 12:07:04 +0300

    Update API URL in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
     production: true,
-    // ⚠️ IMPORTANT : Mettre à jour cette URL avec l'URL de votre backend Railway
-    // Exemple : https://votre-app.up.railway.app/api
-    // Ou si vous utilisez un domaine : https://api.votre-domaine.com/api
-    apiUrl: 'https://votre-backend.up.railway.app/api',
+    apiUrl: 'https://m1p13meanfinaritrafanyah.vercel.app/api',
     googleClientId: '84325736611-egnic0g66qi55rejqcelkgp0bcaie2st.apps.googleusercontent.com'
 };
