218aa2c02095a30eb53fda529feccaeb60708109
Fy Raza <gasyevolve@gmail.com>
2026-03-02 10:53:30 +0300

    Change server listen address to '0.0.0.0'

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,6 +32,6 @@ connectDB().then(async () => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
